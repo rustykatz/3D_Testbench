060320ccd156241ad830344f5c77656e0063c4c6
Rusty <rww6@sfu.ca>
2019-01-17 15:59:38 -0800

    Adding Repo

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour {
+
+    private GameObject _Enemy;
+	// Use this for initialization
+	void Awake () {
+        _Enemy = GameObject.FindGameObjectWithTag("Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("Enemy"))
+        {
+
+            _Enemy.GetComponent<EnemyHealth>().TakeDamage(1);
+
+            //Destroy(coll.gameObject);
+            //ITakeDamage damagable = Enemy.collider.GetComponent<ITakeDamage>();
+            Destroy(gameObject);
+
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyHealth : MonoBehaviour {
+
+
+    private int health = 3;
+    // Display Enemy Test health in UI
+    public Text health_txt; 
+
+
+
+	// Use this for initialization
+    void Start () {
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        health_txt.text = "Enemy Health: " + health.ToString(); 
+	}
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //void OnTriggerEnter(Collider coll)
+    //{
+    //    if (coll.gameObject.CompareTag("Projectile"))
+    //    {
+    //        Destroy(coll.gameObject);
+    //        Destroy(gameObject);
+
+    //    }
+    //}
+}
